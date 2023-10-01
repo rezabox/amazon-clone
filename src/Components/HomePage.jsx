580f48fc729e6c8bd467c5bb8f7f428b19cb3247
rezabox <reza.asareh81@gmail.com>
2023-10-01 10:29:20 +0330

    update responsive the HomePage

--- src/Components/HomePage.jsx
+++ src/Components/HomePage.jsx
@@ -1,17 +1,36 @@
 import React from 'react'
 import Carousel from './Carousel';
-
+import HomePageCard from './HomePageCard';
+import Image1 from '../images/home_grid_1.jpg';
+import Image2 from '../images/home_grid_2.jpg';
+import Image3 from '../images/home_grid_3.jpg';
+import Image4 from '../images/home_grid_4.jpg';
 const HomePage = ()=> {
   return (
     <div className='bg-amazonclone-background'>
-        <div className='min-w-[1000px] max-w-[1500px] m-auto bg-purple-500'>
+        <div className='sm:min-w-[1000px] sm:max-w-[1500px] m-auto bg-purple-500 max-w-[500px]'>
               Testing
               <Carousel/>
-              <div className='grid grid-cols-3 xl:grid-cols-4'>
-                  <div>1</div>
-                  <div>2</div>
-                  <div>3</div>
-                  <div>4</div>
+              <div className='grid md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4'>
+                  <HomePageCard title={"we have a surprise for you"}
+                    img={Image1}
+                    link={"See terms and conditions"}
+                  />
+                  <HomePageCard
+                   title={"Watch The Rings of Power"}
+                   img={Image2}
+                   link={"Start streaming now"}
+                   />
+                  <HomePageCard
+                   title={"Unlimited Streaming"}
+                   img={Image3}
+                   link={"Find out more"}
+                   />
+                  <HomePageCard
+                  title={"More titles to explore"}
+                  img={Image4}
+                  link={"Browse Kindle Unlimited"}
+                  />
               </div>
         </div>
     </div>
