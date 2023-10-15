d68e3e365a91d0a3f7131afb8dd9904f998b7948
rezabox <reza.asareh81@gmail.com>
2023-10-15 08:45:44 +0330

    update adminbutton

--- src/Components/adminOnlyRoute/AdminOnlyRoute.js
+++ src/Components/adminOnlyRoute/AdminOnlyRoute.js
@@ -1,16 +1,24 @@
 import React from "react";
 import { useSelector } from "react-redux";
-import { useNavigate } from "react-router-dom";
+import { Link } from "react-router-dom";
 import { selectEmail } from "../../redux/slice/authSlice";
 
 const AdminOnlyRoute = ({ children }) => {
       const userEmail = useSelector(selectEmail);
-      const nav = useNavigate();
       if(userEmail === "reza.asareh81@gmail.com") {
           return children;   
       }
       return(
-          nav('/')
+        <section>
+        <div className="grid items-center justify-center mt-[10%] space-y-3">
+          <h2 className="font-bold">Permission Denied.</h2>
+          <p>This page can only be view by an Admin user.</p>
+          <br />
+          <Link to="/">
+            <button className="--btn text-orange-500 hover:font-bold">&larr; Back To Home</button>
+          </Link>
+        </div>
+      </section>
       );
 };
 
