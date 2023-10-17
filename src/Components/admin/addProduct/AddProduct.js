6d37e890fcea7e7eb0df7ee008452adafb30089e
rezabox <reza.asareh81@gmail.com>
2023-10-17 12:00:31 +0330

    add home and order and viewProduct

---
+++ src/Components/admin/addProduct/AddProduct.js
@@ -0,0 +1,8 @@
+const AddProduct = () => {
+    return(
+        <>
+          <h1>add in product</h1>
+        </>
+    )
+}
+export default AddProduct;
---
+++ src/Components/admin/viewProducts/ViewProduct.js
@@ -0,0 +1,8 @@
+const ViewProducts = () => {
+    return(
+        <div>
+            <h1>View Product</h1>
+        </div>
+    )
+}
+export default ViewProducts;
