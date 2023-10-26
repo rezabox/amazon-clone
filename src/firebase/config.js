26f897eab890044ef237849c601b0028d7a82883
rezabox <reza.asareh81@gmail.com>
2023-10-26 21:34:47 +0330

    change the firebase project

--- src/firebase/config.js
+++ src/firebase/config.js
@@ -1,9 +1,10 @@
 // Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
 import { getAuth } from 'firebase/auth';
+import { getFirestore } from "firebase/firestore";
+import { getStorage } from "firebase/storage";
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
-
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
@@ -14,8 +15,9 @@ const firebaseConfig = {
   messagingSenderId: "653235498319",
   appId: "1:653235498319:web:57db1d22591e120912293a"
 };
-
-
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
 export const auth = getAuth(app);
+export const db = getFirestore(app);
+export const storage = getStorage(app);
+export default app;
