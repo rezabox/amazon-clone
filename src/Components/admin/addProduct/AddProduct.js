ffe1169c5f042235b5fd31bcbb8bc34f18adf8fb
rezabox <reza.asareh81@gmail.com>
2023-11-03 12:44:12 +0330

    update 2 addProduct

--- src/Components/admin/addProduct/AddProduct.js
+++ src/Components/admin/addProduct/AddProduct.js
@@ -25,9 +25,17 @@ const initialState = {
    desc: "",
 };
 const AddProduct = () => {
+  const { id } = useParams();
   const [product, setProduct] = useState({
     ...initialState,
   });
+
+  function detectForm(id, f1, f2) {
+      if(id === "ADD"){
+         return f1
+      }
+      return f2
+  }
   const navigate = useNavigate();
   const [uploadProgress, setUploadProgress] = useState(0);
   const handleInputChange = (e) => {
@@ -40,7 +48,7 @@ const AddProduct = () => {
      const uploadTask = uploadBytesResumable(storageRef, file);
      uploadTask.on(
        "state_changed",
-        (snapshot) =>{
+        (snapshot) => {
           const progress = (snapshot.bytesTransferred / snapshot.totalBytes) * 100;
           setUploadProgress(progress);   
         },   
@@ -99,9 +107,9 @@ const AddProduct = () => {
   return(
     <>
       <div className="product w-[100%] md:max-w-[1000px] max-w-[500px]">
-        <h2>Add New Product / Edit Product</h2>
+        <h2>{detectForm(id, "Add New Product", "Edit Product")}</h2>
         <card className="cardNav">
-          <form onSubmit>
+          <form onSubmit={addProduct} className='p-4'>
             <div className="styleLabel">
             <label className="adminLable">Product name:</label>
             <input
@@ -109,9 +117,10 @@ const AddProduct = () => {
               placeholder="Product name"
               required
               name="name"
-              className="Lableinput"
-            />
-            </div>
+              value={product.name}
+              onChange={(e) => handleInputChange(e)}
+             />
+             </div>
             <div className="styleLabel">
             <label className="adminLable">Product image:</label>
             <card className='max-w-[500px] p-4 '>
@@ -128,7 +137,15 @@ const AddProduct = () => {
                    </div>
               )}
                <input type="file" accept="image/*" placeholder='product image' name='image' onChange={(e) => handleImageChange(e)} />
-               
+               {product.imageURL1 === "" ? null : (
+                  <input
+                     type="file"
+                     placeholder='Image URL1'
+                     name="imageURL"
+                     value={product.imageURL1}
+                     disabled
+                  />
+               )}
             </card>         
             </div>
             <div className="styleLabel">
@@ -138,19 +155,34 @@ const AddProduct = () => {
               placeholder="Product price"
               required
               name="price"
+              value={product.price}
+              onChange={(e) => handleInputChange(e)}
             />
             </div>
             <div className="styleLabel">
              <label className="adminLable">Product Category:</label>
-             <select name="categories" required></select> 
+             <select name="category" required value={product.category} onChange={(e) => handleInputChange(e)}>
+                 <option value="" disabled>
+                   -- choose product category --
+                 </option>
+                 {categories.map((cat) => {
+                      return (
+                        <option key={cat.id} value={cat.name}>
+                             {cat.name}
+                        </option>
+                      );
+                 })}
+            </select> 
             </div>
           <div className="styleLabel">
               <label className="adminLable">Company/Brand:</label>
-              <input type="text" placeholder="Product brand"  className="Lableinput" required name="brand" />
+              <input type="text" placeholder="Product brand"  className="Lableinput" required name="brand" value={product.brand} onChange={(e) => handleInputChange(e)}/>
           </div>
-          <label>Product Description</label>
-          <textarea name="desc" required  cols="30" rows="10"></textarea>
-          <button className="btn">
+          <div className='flex flex-col p-5'>
+          <label className='text-xl '>Product Description</label>
+          <textarea name="desc" required className='mt-5 border-2 border-slate-500 rounded-md outline-none p-2'  cols="30" rows="10"></textarea>
+          </div>
+          <button className="btn p-5 text-md bg-orange-400 rounded-full">
               Save Product / Edit Product
           </button>
           </form>
