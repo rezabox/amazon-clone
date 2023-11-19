fc475a7409788c7896f444e484e46cdb5d7176b3
rezabox <reza.asareh81@gmail.com>
2023-11-19 23:01:06 +0330

    updated stored

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,11 +1,13 @@
 import { configureStore, combineReducers } from "@reduxjs/toolkit";
 import authSlice from "./slice/authSlice";
 import productSlice from "./slice/productSlice";
+import cartSlice from "./slice/cartSlice";
 
 
 const rootReducer = combineReducers({
     auth: authSlice,
-    product: productSlice
+    product: productSlice,
+    cart: cartSlice
 });
 
 const store = configureStore({
