efdc524ba482cfa4fc656b3d569c2babf3f9ec0e
rezabox <reza.asareh81@gmail.com>
2023-11-25 00:22:39 +0330

    bugfixed and updated the project

--- src/Components/product/productItem/ProductItem.js
+++ src/Components/product/productItem/ProductItem.js
@@ -1,10 +1,13 @@
 import React, { useEffect, useState } from "react";
 import { createDispatchHook, useDispatch, useSelector } from "react-redux";
 import { Link } from "react-router-dom";
-import { ADD_ITEM_INDEX, CALCULATE_TOTAL_QUANTILY } from "../../../redux/slice/cartSlice";
+import {
+  ADD_ITEM_INDEX,
+  CALCULATE_TOTAL_QUANTILY,
+} from "../../../redux/slice/cartSlice";
 import { selectProduct } from "../../../redux/slice/productSlice";
 import Loader from "../../loader/Loader2";
-import styles from './ProductItem.css';
+import styles from "./ProductItem.css";
 
 const ProductItem = ({ product }) => {
   const products = useSelector(selectProduct);
@@ -16,42 +19,50 @@ const ProductItem = ({ product }) => {
     }
     return text;
   };
-  const addToCart = (product)=> {
-      dispatch(ADD_ITEM_INDEX(product));
-      dispatch(CALCULATE_TOTAL_QUANTILY(product))
-  }
+  const addToCart = (product) => {
+    dispatch(ADD_ITEM_INDEX(product));
+    dispatch(CALCULATE_TOTAL_QUANTILY(product));
+  };
 
   return (
-      <>
-         <div className='grid'>
-             {products.length === 0 ? (
-                  <>
-                     <Loader/>
-                  </>
-                ) : (
-               <div className="section">
-                  {products.map((product, index) =>{
-                      const { id, name, price, imageURL1, category } = product
-                      return(
-                       
-                          <list className="list">
-                             <div className='img'>
-                                 <img src={imageURL1} alt={name} />
-                             <div className="content">
-                              <div className="details">
-                                  <p>{`$${price}`}</p>
-                                  <h3>{shortenText(name, 18)}</h3>
-                                <button className="btn cursor-pointer" onClick={()=> addToCart(product)}>Add to Cart</button>
-                              </div>
-                             </div>
-                             </div>
-                          </list>
-                      )
-                  })}
-               </div>
-             )}
-         </div>
-      </>
+    <>
+      <div className="grid">
+        {products.length === 0 ? (
+          <>
+            <Loader />
+          </>
+        ) : (
+          <div className="section">
+            {products.map((product, index) => {
+              const { id, name, price, imageURL1, category } = product;
+              return (
+                <list className="list">
+                  <div className="img">
+                    <Link to={`/product-details/${id}`}>
+                      <div className="image_product cursor-pointer">
+                        <img src={imageURL1} alt={name} />
+                      </div>
+                    </Link>
+                    <div className="content">
+                      <div className="details">
+                        <p>{`$${price}`}</p>
+                        <h3>{shortenText(name, 18)}</h3>
+                        <button
+                          className="btn cursor-pointer"
+                          onClick={() => addToCart(product)}
+                        >
+                          Add to Cart
+                        </button>
+                      </div>
+                    </div>
+                  </div>
+                </list>
+              );
+            })}
+          </div>
+        )}
+      </div>
+    </>
   );
 };
 
---
+++ src/Components/product/productList/ProductList.js
@@ -0,0 +1,24 @@
+import { useEffect, useState } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import ProductItem from "../productItem/ProductItem";
+import { selectFilterProducts } from "../../../redux/slice/filterSlice";
+
+const ProductList = ({ products }) => {
+  const filterProducts = useSelector(selectFilterProducts);
+   return(
+       <div className="">
+          {products.length === 0 ? (<p>No product found.</p>) : (
+              <>
+                {filterProducts.map((product) => {
+                     return(
+                        <div key={product.id}>
+                           <ProductItem {...product} product={product} />
+                        </div>
+                     )
+                })}
+              </>
+          )}
+       </div>
+   ) 
+}
+export default ProductList;
