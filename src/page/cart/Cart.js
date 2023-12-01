65163d570286bd8a0fca757a2408ac9148c423cf
rezabox <reza.asareh81@gmail.com>
2023-12-01 12:05:40 +0330

    updated Cart.js and create table

--- src/page/cart/Cart.js
+++ src/page/cart/Cart.js
@@ -29,10 +29,91 @@ const Cart = ()=> {
        dispatch(CALCULATE_SUBTOTAL());
        dispatch(CALCULATE_TOTAL_QUANTILY());
        dispatch(SAVE_URL());
-  },[cartItems, dispatch])
+  },[cartItems, dispatch]);
+
+  const url = window.location.href;
+  const checkout = ()=>{
+      if(isLoggedIn){
+          navigate('/');
+      }else{
+          dispatch(SAVE_URL(url));
+          navigate('/login');
+      }
+  }
+
   return(
       <section>
-
+        <div className="container">
+            <h2>Shopping Cart</h2>
+            {cartItems.length === 0 ? (
+                <>
+                  <p>your cart is currently empty.</p>
+                  <br/>
+                  <div>
+                      <Link to='/'>&larr; Continue Shopping</Link>
+                  </div>
+                </>
+            ):(
+              <>
+               <table>
+                <thead>
+                  <tr>
+                    <th>s/n</th>
+                    <th>Product</th>
+                    <th>Price</th>
+                    <th>Quantity</th>
+                    <th>Total</th>
+                    <th>Action</th>
+                  </tr>
+                </thead>
+                {cartItems.map((cart, index) => {
+                   const { id, name, price, imageURL1, cartQuantity } = cart;
+                   return (
+                    <>
+                      <tbody key={id}>
+                         <tr>
+                            <td>{index + 1}</td>
+                            <td>
+                              <p>
+                                 <b>{name}</b>
+                              </p>
+                              <img 
+                               src={imageURL1}
+                               alt={name}
+                               style={{ width: "100px" }}
+                               />
+                            </td>
+                            <td>
+                              {price}
+                            </td>
+                            <td>
+                              <div className="count">
+                                <button
+                                  className="--btn"
+                                  onClick={() => decreaseCart(cart)}
+                                >
+                                  -
+                                </button>
+                                <p>
+                                    <b>{cartQuantity}</b>
+                                </p>
+                                <button
+                                  className="--btn"
+                                  onClick={() => increaseCart(cart)}
+                                >
+                                  +
+                                </button>
+                              </div>
+                            </td>
+                         </tr>
+                      </tbody>
+                    </>  
+                   )
+                })}
+               </table>
+              </>
+            )}
+        </div>
       </section>
   )
 }
